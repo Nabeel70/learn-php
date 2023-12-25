cfc9a1d28b301b8c3782289e7e6e9dc50b3a2e79
Nabeel70 <amin70rb@gmail.com>
2023-12-25 10:41:16 +0500

    Getting started

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learning PHP 2023-24</title>
+</head>
+<body>
+    <h1>Hi, I'm going to learn PHP again,(hihihihihi)</h1>
+    <p>But I'm pretty Confident that this time I'll make an impact and will get a job Insh-Allah </p>
+    <?php
+    /*This is the multiline Comment */
+    echo "<h1>Hi PHP</h1>";
+    ?>
+</body>
+</html>
