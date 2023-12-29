ac511429e6895314f3444ab92b61a965c9778ba2
Nabeel70 <amin70rb@gmail.com>
2023-12-29 21:15:11 +0500

    Declaring, Adding and replacing a value in the array

--- basics/array.php
+++ basics/array.php
@@ -6,6 +6,22 @@
     <title>Arrays in PHP</title>
 </head>
 <body>
+    
+<?php
+    //There are 2 ways to declare or write arrays in PHP
+    //1st way to declare
+    $cars = array("Tesla", "Honda", "Fraari", "BMW");
+    //2nd way to declare
+    $fruits = ["Apple", "Banana", "Pineapple", "Cherry"];
+
+    // Here how we can add value in array
+    $fruits[] = "Orange";
+    // echo $fruits[4];
+    
+    //Replace the value "Apple" with the "Kiwi"
+    $fruits[0] = "Kiwis";
+    echo $fruits[0];
+    ?>
 
 </body>
 </html>
