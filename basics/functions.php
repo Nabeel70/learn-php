28ec9148275d7148f7a078a83a4d620b57feffca
Nabeel70 <amin70rb@gmail.com>
2023-12-30 11:15:41 +0500

    Added replace the word in a string function

--- basics/functions.php
+++ basics/functions.php
@@ -17,7 +17,11 @@
     //Know the location/index value of specific character in a string
     $brand = "Apple is a global company";
     echo strpos($brand, "c");
+    echo "<br>";
 
+    //Replace the word in a string 
+    $greetings = "Welcome to Web Course";
+    echo str_replace("Web", "PHP", $greetings);
 
     ?>
 </body>
