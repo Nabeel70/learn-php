2f653bb6136a835b6962d192875a1feefc7b8fa9
Nabeel70 <amin70rb@gmail.com>
2023-12-30 12:28:21 +0500

    Added Explode function

--- basics/functions.php
+++ basics/functions.php
@@ -22,6 +22,27 @@
     //Replace the word in a string 
     $greetings = "Welcome to Web Course";
     echo str_replace("Web", "PHP", $greetings);
+    echo "<br>";
+
+    //Convert the string into lower case
+    $string1 = "Just the random string";
+    echo strtolower($string1);
+    echo "<br>";
+
+    //Convert the string into upper case
+    $string2 = "Check th string to upper";
+    echo strtoupper($string2);
+    echo "<br>";
+
+    //Get specific indexed values from string
+    $string3 = "Hi This is the test string";
+    echo substr($string3, 2, -3);
+    echo "<br>";
+
+    //Explode the string or add something between characters of string
+    $string4 = "Th is is t he test str ing for explode function";
+    print_r(explode(" ", $string4));
+
 
     ?>
 </body>
