fce15b9047d760f9e201cb157a48f5871de3f895
Nabeel70 <amin70rb@gmail.com>
2023-12-31 21:05:34 +0500

    Added global and local variable

--- basics/scope.php
+++ basics/scope.php
@@ -9,7 +9,24 @@
     <?php 
 
     //Global Variables
-    
+    $global_var = "Hi, This is Global varibale";
+    // Using global variable
+    echo $global_var;
+    echo "<br>";
+
+
+    //Local Variable Variables
+    function myFunction()
+    {
+        //Define a local variable
+        $localvar = "Hi, This is Local varibale";
+
+        //Use Local Variable
+        return $localvar; //Local Variable(We 
+        // can't use/access it outside of this function)
+    }
+    echo myFunction();
+
     ?>
     
 </body>
