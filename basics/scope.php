607141a49162de565b0dddafe433eddfc89c5b0b
Nabeel70 <amin70rb@gmail.com>
2023-12-31 21:22:36 +0500

    Static scope in php

--- basics/scope.php
+++ basics/scope.php
@@ -11,7 +11,7 @@
     //Global Variables
     $global_var = "Hi, This is Global varibale";
     // Using global variable
-    echo $global_var;
+    // echo $global_var;
     echo "<br>";
 
 
@@ -22,10 +22,33 @@
         $localvar = "Hi, This is Local varibale";
 
         //Use Local Variable
+        return $GLOBALS["global_var"];
+        echo "<br>";
         return $localvar; //Local Variable(We 
         // can't use/access it outside of this function)
     }
-    echo myFunction();
+    // echo myFunction();
+
+    /*Static Variable(Use to store the old value of 
+    funtion and add next value to it when call the function again) */
+    function staticFunction()
+    {
+        //Declare a static variable
+        static $staticvar = 0;
+
+        //Increment the static variable value
+        $staticvar++;
+
+        //Use the static variable
+        return $staticvar;
+    }
+    echo staticFunction();
+    echo "<br>";
+    echo staticFunction();
+    echo "<br>";
+    echo staticFunction();
+    echo "<br>";
+    echo staticFunction();
 
     ?>
     
