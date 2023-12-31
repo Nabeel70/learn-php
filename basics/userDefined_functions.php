5c83861a6023cbe65411b4067045f33931a49253
Nabeel70 <amin70rb@gmail.com>
2023-12-31 16:34:18 +0500

    created custom function

---
+++ basics/userDefined_functions.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User defined funtions in PHP</title>
+</head>
+<body>
+    <?php
+    function sayHello(){
+        echo "Hello My function";
+    }
+
+    return sayHello();
+
+
+    ?>
+</body>
+</html>
