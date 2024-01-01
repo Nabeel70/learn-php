9044ea7f3d0c0bd2bc86437366eac4047292545c
Nabeel70 <amin70rb@gmail.com>
2024-01-01 21:15:29 +0500

    For loop in php

---
+++ basics/loop.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loops in PHP</title>
+</head>
+<body>
+    <?php
+    //For loop
+    for($i = 0; $i<=5; $i++){
+        echo "Hi there..!<br>";
+    }
+
+    
+    ?>
+    
+</body>
+</html>
