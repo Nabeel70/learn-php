3100fb43e6adcdb45a509ef15ed1ed63a2e744c0
Nabeel70 <amin70rb@gmail.com>
2024-01-03 20:56:10 +0500

    Update table data

--- basics/db.sql
+++ basics/db.sql
@@ -2,3 +2,4 @@ INSERT INTO users (username, pwd, email) VALUES
 ('Muhammad Nabeel Amin', 'admmin', 'admin@gmail.com');
 INSERT INTO users (username, pwd, email) VALUES 
 ('Aqeel', 'admin', 'admin123@gmail.com');
+UPDATE users SET username = 'Muhammad Aqeel', email = 'admin456@gmail.com WHERE id = 2;
