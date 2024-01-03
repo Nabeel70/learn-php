5d530b62e8ad68746bc77cf7235c8df9bc4b67b8
Nabeel70 <amin70rb@gmail.com>
2024-01-03 20:51:12 +0500

    Insert 2nd query

--- basics/db.sql
+++ basics/db.sql
@@ -1,2 +1,4 @@
 INSERT INTO users (username, pwd, email) VALUES 
 ('Muhammad Nabeel Amin', 'admmin', 'admin@gmail.com');
+INSERT INTO users (username, pwd, email) VALUES 
+('Aqeel', 'admin', 'admin123@gmail.com');
