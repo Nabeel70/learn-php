68928cce4d7d960fc5b04e30d41729704034fa37
Nabeel70 <amin70rb@gmail.com>
2024-01-04 10:27:52 +0500

    Created Form in database file

--- basics/database.php
+++ basics/database.php
@@ -4,10 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Database in PHP</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php
-    
-    ?>
+    <h3>Signup</h3>
+
+    <form action="../includes/dbh.inc.php">
+        <input type="text" name="username" placeholder="Username"><br/><br/>
+        <input type="password" name="pwd" placeholder="Password"><br/><br/>
+        <input type="text" name="email" placeholder="Email"><br/><br/>
+        <button>Signup</button>
+    </form>
+   
 </body>
 </html>
