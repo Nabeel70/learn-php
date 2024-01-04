5493f329bafa5813a6dbfea2acf00bff59164685
Nabeel70 <amin70rb@gmail.com>
2024-01-04 09:23:26 +0500

    Added try and catch code in dbh.inc

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -3,3 +3,11 @@
 $dsn = "mysql:host=localhost;dbname=php_db";
 $dbusername = "root";
 $dbpassword = "";
+
+try {
+    //PHP Data Object
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
