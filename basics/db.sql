7115e3d0748a9dc873ea26e696c2fe1908c35a48
Nabeel70 <amin70rb@gmail.com>
2024-01-04 07:16:48 +0500

    select username and email query

--- basics/db.sql
+++ basics/db.sql
@@ -6,3 +6,4 @@ UPDATE users SET username = 'Muhammad Aqeel', email = 'admin456@gmail.com WHERE 
 DELETE from users WHERE id = 2;
 INSERT INTO comments (username, comment_text, user_id) VALUES 
 ('Muhammad Nabeel Amin', 'Hi, This is a Test Comment', 1);
+SELECT username, email FROM users WHERE id = 1;
