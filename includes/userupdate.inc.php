f1adef85331cda00dfdbb27c56eb674ae80b0191
Nabeel70 <amin70rb@gmail.com>
2024-01-05 17:20:29 +0500

    Added userupdate file

---
+++ includes/userupdate.inc.php
@@ -0,0 +1,44 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $username = $_POST["username"];
+    $password = $_POST["pwd"];
+    $email = $_POST["email"];
+
+    try {
+        require_once "dbh.inc.php";
+
+        //prepare parameters
+        // $query = "INSERT INTO users (username, pwd, email) 
+        // VALUES(?, ?, ?);";
+
+        // $stmt = $pdo ->prepare($query);
+
+        // $stmt->execute([$username,$password, $email]);
+
+        //Named parameters
+        $query = "INSERT INTO users (username, pwd, email) 
+        VALUES(:username, :pwd, :email);";
+
+        $stmt = $pdo ->prepare($query);
+
+        $stmt->bindParam(":username", $username);
+        $stmt->bindParam(":pwd", $password);
+        $stmt->bindParam(":email", $email);
+
+        $stmt->execute();
+
+        //Close the PDO connection
+        $pdo = null;
+        $stmt = null;
+        header("Location:../../../learn-php/basics/database.php");
+
+        exit();
+    } catch (PDOException $e) {
+        die("Query failed: " . $e->getMessage());
+    }
+
+} else{
+    header("Location:../../../learn-php/basics/database.php");
+}
