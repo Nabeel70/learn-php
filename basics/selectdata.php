f037b96ea343294f64f8f1c613a79054c7b5f7fb
Nabeel70 <amin70rb@gmail.com>
2024-01-06 09:31:06 +0500

    added style.css file in selectdata.php

--- basics/selectdata.php
+++ basics/selectdata.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Selecting Data PHP</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     
     <form class="searchform" action="search.php" method="post">
         <label for="search">Search for user:</label>
-        <input type="text" name="usersearch" id="search" placeholder="Search">
-        <button>Search</button>
+        <input type="text" name="usersearch" id="search" placeholder="Search...">
+        <button class="btn-signup">Search</button>
     </form>
 </body>
 </html>
