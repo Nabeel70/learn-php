de40d90cf616349c7361ef55703cb6b5394a3aee
Nabeel70 <amin70rb@gmail.com>
2024-01-06 11:59:40 +0500

    Setup search page query and code for search results

--- projects/search.php
+++ projects/search.php
@@ -1,3 +1,38 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $userSearch = $_POST["usersearch"];
+
+    try {
+        require_once "../includes/dbh.inc.php";
+
+        //Named parameters
+        $query = "SELECT * FROM comments WHERE username = :usersearch;";
+
+        $stmt = $pdo ->prepare($query);
+
+        $stmt->bindParam(":usersearch", $userSearch);
+
+        $stmt->execute();
+
+        $results = $stmt->fetchAll(pdo::FETCH_ASSOC);
+
+        //Close the PDO connection
+        $pdo = null;
+        $stmt = null;
+        
+    } catch (PDOException $e) {
+        die("Query failed: " . $e->getMessage());
+    }
+
+} else{
+    header("Location:../../../learn-php/projects/selectdata.php");
+}
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,9 +42,21 @@
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
+    <h3>Search Results: </h3>
 
     <?php
-    
+    if(empty($results)){
+        echo "<div>";
+        echo "<p>Sorry..!There were no Results..!</p>";
+        echo "</div>";
+    }else{
+        foreach($results as $row){
+            echo htmlspecialchars($row["username"]);
+            echo htmlspecialchars($row["comment_text"]);
+            echo htmlspecialchars($row["created_at"]);
+        }
+    };
+
     ?>
 </body>
 </html>
