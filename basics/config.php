cf6f91bbf489986c037b75dc6079dbab97e26f0e
Nabeel70 <amin70rb@gmail.com>
2024-01-07 09:20:58 +0500

    Setup session cookie parameters

--- basics/config.php
+++ basics/config.php
@@ -3,3 +3,12 @@
 ini_set('session.use_only_cookies',  1);
 //Use strict mode for session(it's mandatory when we want to deals with cookies sessions)
 ini_set('session.use_strict_mode',  1);
+
+//Setup session cookie parameters
+session_set_cookie_params([
+    'lifetime' => 1800,
+    'domain' => 'localhost',
+    'path' => '/',
+    'secure' => true,
+    'httponly' => true,
+]);
