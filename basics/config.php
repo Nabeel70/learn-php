10f34863662a752f1bfe36cf1289234cf8632bd4
Nabeel70 <amin70rb@gmail.com>
2024-01-07 09:39:50 +0500

    Regenrate the session ID

--- basics/config.php
+++ basics/config.php
@@ -16,3 +16,17 @@ session_set_cookie_params([
 //Starting the session
 session_start();
 
+//Regenrate the session ID after every 30 minutes for security reasons  
+if(!isset($_SESSION['last_regeneration'])) {
+
+    session_regenerate_id(true);
+    $_SESSION['last_regeneration'] = time();
+} else {
+
+    $interval = 60 * 30;
+
+    if (time() - $_SESSION['last_regeneration'] >= $interval) {
+        session_regenerate_id(true);
+        $_SESSION['last_regeneration'] = time();
+    }
+}
