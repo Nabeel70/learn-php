335ace1cd91b77028ff29c69bc3a3f3e6c37a330
Nabeel70 <amin70rb@gmail.com>
2024-01-07 11:19:53 +0500

    check hash data are same or not

--- includes/hashpwd.inc.php
+++ includes/hashpwd.inc.php
@@ -10,3 +10,22 @@ $dataToHash = $senstiveData . $salt . $pepper;
 $hash = hash("sha256", $dataToHash);
 
 echo "<br>" . $hash;
+
+/*  Recreate hashing and check the data*/
+
+$senstiveData = "Nabeel";
+$storedSalt = $salt;
+$storedHash = $hash;
+$pepper = "BhaiSecretPapperStrington";
+
+$dataToHash = $senstiveData . $storedSalt . $pepper;
+
+$verification = hash("sha256", $dataToHash);
+
+//Now let's check by if-else block
+
+if($storedHash === $verification){
+    echo "<br>The data are the same"; 
+} else{
+    echo "<br>The data are not the same"; 
+}
