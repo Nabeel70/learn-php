dddc7c69536da87b60d4794bc05932911870737b
Nabeel70 <amin70rb@gmail.com>
2024-01-08 09:44:23 +0500

    Setup Session generation timestamp

--- projects/Login&Signup System/include/dbh.inc.php
+++ projects/Login&Signup System/include/dbh.inc.php
@@ -1,10 +1,12 @@
 <?php
 
+//Define variables for database connection
 $host = "localhost";
 $dbname = "php_db";
 $dbusername = "root";
 $dbpassword = '';
 
+//setup database connection
 try {
     //PHP data objects   
     $pdo = new PDO("mysql:host = $host; dbname = $dbname",
@@ -12,4 +14,23 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection Error: " . $e->getMessage());
+}
+
+//Start the session
+session_start();
+
+//Create a function for regenrate session id 
+function regenrate_session_id(){
+    session_regenerate_id();
+    $_SESSION['last_regeneration'] = time();
+}
+
+//Create If-else statement for regenrate session if not already created
+if(!isset($_SESSION['last_regeneration'])){
+    regenrate_session_id();
+} else{
+    $interval = 60 * 30;
+    if( time() - $_SESSION['last_regeneration'] >= $interval){
+        regenrate_session_id();
+    }
 }
