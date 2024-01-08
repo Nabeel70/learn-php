2807458c0b69c3a024c3660b8fe7de5170e56b81
Nabeel70 <amin70rb@gmail.com>
2024-01-08 09:22:48 +0500

    Setup Connection to db

--- projects/Login&Signup System/include/dbh.inc.php
+++ projects/Login&Signup System/include/dbh.inc.php
@@ -1,1 +1,15 @@
 <?php
+
+$host = "localhost";
+$dbname = "php_db";
+$dbusername = "root";
+$dbpassword = '';
+
+try {
+    //PHP data objects   
+    $pdo = new PDO("mysql:host = $host; dbname = $dbname",
+     $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection Error: " . $e->getMessage());
+}
