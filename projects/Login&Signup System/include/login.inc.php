0701b11c396086b53682f353031f6abec257e496
Nabeel70 <amin70rb@gmail.com>
2024-01-16 08:52:05 +0500

    link mvc files in login file

--- projects/Login&Signup System/include/login.inc.php
+++ projects/Login&Signup System/include/login.inc.php
@@ -4,7 +4,12 @@ if($_SERVER['REQUEST_METHOD'] == 'post') {
     $username = $_POST["username"];
     $password = $_POST["pwd"];
     try {
-        
+        require_once 'dbh.inc.php';
+        require_once '../model/login_model.inc.php';
+        // require_once '../view/login_view.inc.php';
+        require_once '../controller/login_controller.inc.php';
+
+
     } catch (PDOException $e) {
         echo "Query Failed:  " . $e->getMessage();
     }
