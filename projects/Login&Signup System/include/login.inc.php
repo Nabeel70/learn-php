9903fa0ba8d3037427ea16584f4247044be1b8bd
Nabeel70 <amin70rb@gmail.com>
2024-01-16 08:47:52 +0500

    setup try catch block

--- projects/Login&Signup System/include/login.inc.php
+++ projects/Login&Signup System/include/login.inc.php
@@ -3,6 +3,16 @@
 if($_SERVER['REQUEST_METHOD'] == 'post') {
     $username = $_POST["username"];
     $password = $_POST["pwd"];
-}
+    try {
+        
+    } catch (PDOException $e) {
+        echo "Query Failed:  " . $e->getMessage();
+    }
 
+
+
+}else {
+    header("Location: ../index.php");
+    die();
+}
 ?>
