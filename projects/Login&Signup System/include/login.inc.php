fddc1cfe2271ea29b934c8df95a08ba8c2737ab7
Nabeel70 <amin70rb@gmail.com>
2024-01-16 09:01:08 +0500

    error handlers function added

--- projects/Login&Signup System/include/login.inc.php
+++ projects/Login&Signup System/include/login.inc.php
@@ -9,6 +9,31 @@ if($_SERVER['REQUEST_METHOD'] == 'post') {
         // require_once '../view/login_view.inc.php';
         require_once '../controller/login_controller.inc.php';
 
+          //Error Handlers
+          $errors = [];
+
+          if(is_input_empty($username, $password, $email)){
+             $errors["empty_input"] = "Fill in all fields!";
+          }
+         
+         //Link to config file for start the session
+         require_once 'config_session.inc.php';
+        
+ 
+         if($errors){
+             $_SESSION["errors_signup"] = $errors;
+ 
+             $signup_data = [
+                 "username" => $username,
+                 "email" => $email
+             ];
+ 
+             $_SESSION["signup_data"] = $signup_data;
+             
+ 
+             header("Location: ../index.php");
+             die();
+         } 
 
     } catch (PDOException $e) {
         echo "Query Failed:  " . $e->getMessage();
