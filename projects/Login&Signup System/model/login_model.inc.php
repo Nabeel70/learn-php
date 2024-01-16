59bce59ceaa4e613ec1b41115819c2d93583c20b
Nabeel70 <amin70rb@gmail.com>
2024-01-16 20:55:48 +0500

    setup login_model file

--- projects/Login&Signup System/model/login_model.inc.php
+++ projects/Login&Signup System/model/login_model.inc.php
@@ -5,22 +5,9 @@ declare(strict_types= 1);
 function get_user(object $pdo, string $username) 
 {
 
-    $query = "SELECT username FROM users WHERE username = :username;";
+    $query = "SELECT * FROM users WHERE username = :username;";
     $stmt = $pdo->prepare($query);
     $stmt->bindParam(":username", $username);
-    $stmt->execute();
-
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    return $result;
-}
-
-//Get the email from the database
-
-function get_email(object $pdo, string $email)
-{
-    $query = "SELECT username FROM users WHERE email = :email;";
-    $stmt = $pdo->prepare($query);
-    $stmt->bindParam(":email", $email);
     $stmt->execute();
 
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
