46275b7fc13feb9b6df1f45b17ed915cd7b44783
Nabeel70 <amin70rb@gmail.com>
2024-01-21 21:12:46 +0500

    view login session

--- projects/Login&Signup System/view/login_view.inc.php
+++ projects/Login&Signup System/view/login_view.inc.php
@@ -2,14 +2,20 @@
 
 declare(strict_types=1);
 
-function check_login_errors(){
-    $errors = $_SESSION["errors_login"];
+function check_login_errors()
+{
+    if(isset($_SESSION["errors_login"])){
+        $errors = $_SESSION["errors_login"];
 
-    echo "<br>";
-
-    foreach($errors as $error){
-        echo '<p>'.$error.'</p>';
+        echo "<br>";
+    
+        foreach($errors as $error){
+            echo '<p>'.$error.'</p>';
+        }
+        unset($_SESSION["errors_login"]);
     }
-
-    unset($_SESSION["errors_login"]);
+    else if (isset($_GET["login"]) && $_GET["login"] === "success"){
+        
+    }
+    
 }
