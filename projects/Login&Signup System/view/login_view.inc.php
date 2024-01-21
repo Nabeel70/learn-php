f41af50f87fdb808e839e58d9c0fe3ef0dfd0637
Nabeel70 <amin70rb@gmail.com>
2024-01-21 12:42:43 +0500

    Added errors code in view file

--- projects/Login&Signup System/view/login_view.inc.php
+++ projects/Login&Signup System/view/login_view.inc.php
@@ -3,5 +3,13 @@
 declare(strict_types=1);
 
 function check_login_errors(){
+    $errors = $_SESSION["errors_login"];
 
+    echo "<br>";
+
+    foreach($errors as $error){
+        echo '<p>'.$error.'</p>';
+    }
+
+    unset($_SESSION["errors_login"]);
 }
